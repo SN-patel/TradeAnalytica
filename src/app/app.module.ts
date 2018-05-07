ba4bf0c6216c1347d3c33a4450b202913c43a0a3
SN-patel <snpatel.jsg@gmail.com>
2018-05-07 09:42:47 +0530

    TD-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
+
+// import {ToolbarModule} from 'primeng/toolbar';
+// import {ButtonModule} from 'primeng/button';
+// import {SplitButtonModule} from 'primeng/splitbutton';
+// import {CardModule} from 'primeng/card';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    LeftnavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    // ToolbarModule,
+    // ButtonModule,
+    // SplitButtonModule,
+    // CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
